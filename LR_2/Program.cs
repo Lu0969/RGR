cebc92683453b4c6c31a74038673d7e6ae61d619
Lu0969 <44342841+Lu0969@users.noreply.github.com>
2019-06-08 18:28:03 +0600

    Add files via upload

---
+++ LR_2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR3_Mashini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car volvo = new Car("Volvo", 200, 100000);
+            List<DateTime> oldNissanRepairDate = new List<DateTime>()
+            {
+                new DateTime(2001,12,10),
+                new DateTime(2002, 1, 1),
+                new DateTime(2003, 4, 14),
+                new DateTime(2003, 7, 30)
+            };
+            Car oldNissan = new Car("Nissan", 120, 50000, oldNissanRepairDate);
+
+            volvo.Write();
+            Console.WriteLine();
+            oldNissan.Write();
+
+            Console.ReadKey();
+        }
+    }
+}
